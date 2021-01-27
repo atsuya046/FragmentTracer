8913adf1dec60697aa58973c44c8041d8b284805
atsuya046 <nobuya.oshiro@gmail.com>
2021-01-27 12:41:06 +0900

    init

---
+++ fragmenttracer-navigation/src/main/java/com/atsuya046/fragmenttracer/navigation/FragmentTracerNavigation.kt
@@ -0,0 +1,27 @@
+package com.atsuya046.fragmenttracer.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.fragment.NavHostFragment
+import com.atsuya046.fragmenttracer.FragmentPerformanceTracer
+
+fun Fragment.attachPerformanceTracer() {
+    val fragmentTracer = FragmentPerformanceTracer()
+    this.lifecycle.addObserver(object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) = when(event) {
+            Lifecycle.Event.ON_CREATE -> fragmentTracer.start(childFragmentManager)
+            Lifecycle.Event.ON_DESTROY -> fragmentTracer.stop(childFragmentManager)
+            else -> {
+                // nothing to do
+            }
+        }
+    })
+}
+
+class FragmentTraceableNavHostFragment: NavHostFragment() {
+    init {
+        this.attachPerformanceTracer()
+    }
+}
